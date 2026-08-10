9bef2151fac6a7fad2b92d7dbc4bc63b4ddb9b4f
ILoveCheeseburgers <152164506+zamzam0303@users.noreply.github.com>
2026-08-10 09:44:42 +0800

    Add files via upload

---
+++ Q1_Acacia_FA1_Blah.py
@@ -0,0 +1,12 @@
+import math
+x1 = float(input("Enter x1: "))
+x2 = float(input("Enter x2: "))
+y1 = float(input("Enter y1: "))
+y2 = float(input("Enter y2: "))
+distance = math.sqrt(math.pow(x2-x1,2) + math.pow(y2-y1,2))
+print(f"\n The distance between the two points is: {distance:.2f}")
+
+#Reflection
+"""""
+Using a Library is more reliable and practical than writing all calculations from scratch. Because it provides a more easier and faster way of calculating equations. It also helps reduce errors and mistakes in solving, making the program simpler and reliable.
+"""
