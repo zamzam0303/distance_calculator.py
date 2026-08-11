156ec565f06ab86c56094f96cdb123501942ffd0
ILoveCheeseburgers <152164506+zamzam0303@users.noreply.github.com>
2026-08-11 09:25:19 +0800

    Refactor distance calculation script for clarity
    Refactor code for clarity and organization, adding comments for input, calculation, and output sections.

--- Q1_Acacia_FA1_Blah.py
+++ Q1_Acacia_FA1_Blah.py
@@ -1,9 +1,15 @@
 import math
+
+# Input Values
 x1 = float(input("Enter x1: "))
 x2 = float(input("Enter x2: "))
 y1 = float(input("Enter y1: "))
 y2 = float(input("Enter y2: "))
-distance = math.sqrt(math.pow(x2-x1,2) + math.pow(y2-y1,2))
+
+# Distance Formula
+distance = math.sqrt(math.pow(x2-x1, 2) + math.pow(y2-y1, 2))
+
+# Output
 print(f"\n The distance between the two points is: {distance:.2f}")
 
 #Reflection
